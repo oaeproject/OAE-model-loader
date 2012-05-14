f6bdffca652003a1eaa7182982d44285c5cf3081
Christian Vuerings <vueringschristian@gmail.com>
2012-05-13 22:29:50 -0400

    Discovery

--- suites/discovery.js
+++ suites/discovery.js
@@ -2,15 +2,17 @@ var general = require("./../api/general.js");
 var suiteAPI = require("./../api/suite.js");
 
 exports.Suite = function(datamodel, SERVER_URL){
-    var id = "recentactivity";
-    var title = "Recent Activity";
+    var id = "discovery";
+    var title = "Discovery";
     var threshold = 90;
     var target = 100;
     var elements = [];
     var runs = [];
 
     // Recent Activity Feed
-    elements.push(new suiteAPI.SuiteElement("recentactivity-data", "Recentactivity Feed", 200, 1000, 0.05, 1));
+    elements.push(new suiteAPI.SuiteElement("recentactivity-data", "Recentactivity Feed", 500, 2000, 0.05, 1));
+
+    elements.push(new suiteAPI.SuiteElement("related-content", "Related Content Feed", 500, 2000, 0.05, 1));
 
     for (var b = 0; b < datamodel.length; b++){
         for (var u = 0; u < datamodel[b].users.length; u++){
@@ -23,6 +25,17 @@ exports.Suite = function(datamodel, SERVER_URL){
                 "params": {
                     "_charset_": "utf-8",
                     "items": 12,
+                    "_": Math.random()
+                }
+            });
+
+            runs.push({
+                "type": "related-content-data",
+                "url": SERVER_URL + "/var/search/pool/me/related-content.json",
+                "user": user,
+                "method": "GET",
+                "params": {
+                    "_charset_": "utf-8",
                     "_": Math.random()
                 }
             });
